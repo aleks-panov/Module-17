3a6dabd958980be4b2e974165937f987eccd0b8e
aleks-panov <aleks_panov@mail.ru>
2025-01-24 12:22:42 +0300

    Add files via upload

--- Django/UrbanDjango/task2/migrations/views.py
+++ Django/UrbanDjango/task2/migrations/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
 # Create your views here.
-def func_template(request):
-    return render(request, 'func_template.html')
+def func_templates(request):
+    return render(request, "func_template.html")
+
+
+class class_templates(TemplateView):
+    template_name = 'class_templates'
